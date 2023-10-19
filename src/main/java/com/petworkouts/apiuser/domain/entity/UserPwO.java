eafbe31031b1444d2aa672e862aacb5d2e892747
Emmanuel Lopera <20448841+Piratadelrock@users.noreply.github.com>
2023-10-19 13:00:03 -0500

    update entity UserPwO

--- src/main/java/com/petworkouts/apiuser/domain/entity/UserPwO.java
+++ src/main/java/com/petworkouts/apiuser/domain/entity/UserPwO.java
@@ -6,6 +6,7 @@ import lombok.*;
 
 import javax.persistence.*;
 
+@Data
 @Entity
 @Table(name = "UserPwO")
 public class UserPwO {
@@ -46,91 +47,4 @@ public class UserPwO {
     @Column(name = "active")
     private boolean active;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getDocumentType() {
-        return documentType;
-    }
-
-    public void setDocumentType(String documentType) {
-        this.documentType = documentType;
-    }
-
-    public Integer getPersonID() {
-        return personID;
-    }
-
-    public void setPersonID(Integer personID) {
-        this.personID = personID;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public PersonGender getGender() {
-        return gender;
-    }
-
-    public void setGender(PersonGender gender) {
-        this.gender = gender;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public UserType getRegisterType() {
-        return registerType;
-    }
-
-    public void setRegisterType(UserType registerType) {
-        this.registerType = registerType;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public boolean isActive() {
-        return active;
-    }
-
-    public void setActive(boolean active) {
-        this.active = active;
-    }
 }
